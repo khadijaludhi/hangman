7e0421fad1c5d62a324238e4fae0128e8215eeac
khadijaludhi <khadijaludhi@gmail.com>
2023-10-21 23:39:25 +0100

    introduced lives to the game

--- hangman.py
+++ hangman.py
@@ -15,9 +15,11 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
     
-    while len(word_letters) > 0:
+    lives = 8
+    
+    while len(word_letters) > 0 and lives > 0:
         # letters that have been used 
-        print('You have already used the following letters: ', ' '.join(used_letters))
+        print('You have', lives, 'lives left. And you have already used the following letters: ', ' '.join(used_letters))
         
         # display the current status of the word
         word_list = [letter if letter in used_letters else '-' for letter in word]
@@ -31,7 +33,16 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else:
+                lives = lives - 1 # removes a life
+                print('Looks like you guessed the wrong letter.')
         else:
             print('Invalid character. Try again :)')
+            
+    # gets here when len(word_letters) == 0 OR when the lives == 0
+    if lives == 0:
+        print('You died... How sad. ANYWAY, the word that cost you your life was', word)
+    else:
+        print('You guessed the word', word, '! Looks like you get to live for just a teensy bit longer :)')
 
 hangman()
